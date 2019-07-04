9e811859877f330561c426ff1d078dd5d3a03e19
Dominic Elm <elmdominic@gmx.net>
2019-07-04 17:23:35 +0200

    fix: formatting issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
